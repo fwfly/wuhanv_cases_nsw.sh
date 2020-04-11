45961754db02e69b290eb4eb0051f9462805beba
fwfly <fwfly@github.com>
2020-04-11 21:32:29 +1000

    Support multiple date format
    suport both dd-mm and mm-dd

--- wuhanv_cases_nsw.sh
+++ wuhanv_cases_nsw.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 postcode=$1
-today=$2
+todaymd=$2
 
 curl -s https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv > nsw_location_cases.csv
 
 curl -s https://data.nsw.gov.au/data/dataset/c647a815-5eb7-4df6-8c88-f9c537a4f21e/resource/2f1ba0f3-8c21-4a86-acaf-444be4401a6d/download/covid-19-cases-by-notification-date-and-likely-source-of-infection.csv > nsw_source_linked.csv
 
 
+
 if [ -z "$2" ];then
     # the update should be delayed one day.
-    today=`date -v -1d +%d-%m`
+    todaydm=`date -v -1d +%d-%m`
+    todaymd=`date -v -1d +%m-%d`
+else
+    mm=`echo $todaymd | awk -F "-" '{print $1}'`
+    dd=`echo $todaymd | awk -F "-" '{print $2}'`
+    todaydm="$dd-$mm"
 fi
 
-new_case=`grep -inr $today nsw_source_linked.csv | wc -l`
-new_postcode_case=`grep -inr $today nsw_location_cases.csv | wc -l`
+echo $todaymd
+new_case=`grep -inr $todaydm nsw_source_linked.csv | wc -l`  # format dd-mm
+new_postcode_case=`grep -inr $todaymd nsw_location_cases.csv | grep $postcode | wc -l` # format mm-dd
 case_num=`grep -inr $postcode nsw_location_cases.csv | wc -l`
 
 echo "New NSW cases: $new_case"
