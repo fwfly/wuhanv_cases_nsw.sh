859833d4cdce65f5edf6fae6aaf331f29a679a38
fwfly <fwfly@github.com>
2020-04-12 15:56:22 +1000

    support cache
    if files exist, the script won't download it.

--- wuhanv_cases_nsw.sh
+++ wuhanv_cases_nsw.sh
@@ -3,10 +3,20 @@
 postcode=$1
 todaymd=$2
 
-curl -s https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv > nsw_location_cases.csv
+filetoday=`date -v -1d +%m-%d`
 
-curl -s https://data.nsw.gov.au/data/dataset/c647a815-5eb7-4df6-8c88-f9c537a4f21e/resource/2f1ba0f3-8c21-4a86-acaf-444be4401a6d/download/covid-19-cases-by-notification-date-and-likely-source-of-infection.csv > nsw_source_linked.csv
+localtion_file="nsw_location_cases-$filetoday.csv"
+source_linked_file="nsw_location_cases-$filetoday.csv"
 
+if [ ! -f "nsw_location_cases-$filetoday.csv" ];
+then
+    # remove old files
+    rm -f nsw_location_cases* nsw_source_linked*
+
+    curl -s https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv > $localtion_file
+
+    curl -s https://data.nsw.gov.au/data/dataset/c647a815-5eb7-4df6-8c88-f9c537a4f21e/resource/2f1ba0f3-8c21-4a86-acaf-444be4401a6d/download/covid-19-cases-by-notification-date-and-likely-source-of-infection.csv > $source_linked_file
+fi
 
 
 if [ -z "$2" ];then
@@ -20,9 +30,9 @@ else
 fi
 
 echo $todaymd
-new_case=`grep -inr $todaydm nsw_source_linked.csv | wc -l`  # format dd-mm
-new_postcode_case=`grep -inr $todaymd nsw_location_cases.csv | grep $postcode | wc -l` # format mm-dd
-case_num=`grep -inr $postcode nsw_location_cases.csv | wc -l`
+new_case=`grep -inr $todaydm $source_linked_file | wc -l`  # format dd-mm
+new_postcode_case=`grep -inr $todaymd $localtion_file | grep $postcode | wc -l` # format mm-dd
+case_num=`grep -inr $postcode $localtion_file | wc -l`
 
 echo "New NSW cases: $new_case"
 echo "New $postcode Cases : $new_postcode_case"
