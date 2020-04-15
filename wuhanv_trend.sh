89df280e6bd742e35269fc16b7e3bc780de187da
fwfly <fwfly@github.com>
2020-04-15 21:51:15 +1000

    Support trend in local area

--- wuhanv_trend.sh
+++ wuhanv_trend.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-dates=`awk -F ',' '{print $1}' nsw_location_cases*  | grep 2020 | uniq`
+if [ -z "$1" ];then
+    dates=`awk -F ',' '{print $1}' nsw_location_cases*  | grep 2020 | uniq`
+else
+    dates=`grep $1 nsw_location_cases* | awk -F ',' '{print $1}' | grep 2020 | uniq`
+fi
+
 
 for day in $dates
 do
-    cases=`grep $day nsw_location_cases* | wc -l`
+    if [ -z "$1" ];then
+        cases=`grep $day nsw_location_cases* | wc -l`
+    else
+        cases=`grep $1 nsw_location_cases* | grep $day | wc -l`
+    fi
     echo "$day $cases"
 done
